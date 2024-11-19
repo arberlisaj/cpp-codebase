8a45ce91f3ded748e6227f6ed7ac2d1063507906
arberlisaj <the.arberlisaj@gmail.com>
2024-11-19 22:56:30 +0100

    initialize `pune_laboratori_nr1`

---
+++ include/pune_laboratori_nr1.h
@@ -0,0 +1,6 @@
+#ifndef PUNE_LABORATORI_NR1_H
+#define PUNE_LABORATORI_NR1_H
+
+int lab1();
+
+#endif
---
+++ src/pune_laboratori_nr1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int lab1() {
+  std::cout << "Pune laboratori nr1" << std::endl;
+  return 0;
+}
