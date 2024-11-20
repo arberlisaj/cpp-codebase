915da3c7c3e53bb6d70e72347faa2d38e1cccdf4
arberlisaj <the.arberlisaj@gmail.com>
2024-11-20 12:37:00 +0100

    complete `pune_laboratori_nr1`

--- include/pune_laboratori_nr1.h
+++ include/pune_laboratori_nr1.h
@@ -1,6 +1,14 @@
 #ifndef PUNE_LABORATORI_NR1_H
 #define PUNE_LABORATORI_NR1_H
 
-int lab1();
+int p1ex01();
+int p1ex02();
+int p1ex03();
+int p1ex04();
+int p1ex05();
+int p1ex06();
+int p1ex07();
+int p1ex08();
+int p1ex09();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include "pune_laboratori_nr3.h"
+#include "pune_laboratori_nr1.h"
 
 int main() {
-  sayHello();
+  p1ex01();
+  p1ex02();
+  p1ex03();
+  p1ex04();
+  p1ex05();
+  p1ex06();
+  p1ex07();
+  p1ex08();
+  p1ex09();
   return 0;
 }
