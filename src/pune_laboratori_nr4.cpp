ad4c73d4395471014e152ae91c393545417277e4
arberlisaj <the.arberlisaj@gmail.com>
2024-11-23 23:48:33 +0100

    chore: set up for solutions

--- src/pune_laboratori_nr4.cpp
+++ src/pune_laboratori_nr4.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
-int lab4() {
-  std::cout << "Hello World" << std::endl;
+using namespace std;
 
+int p4ex01() {
+  cout << "Ushtrimi 1";
+  return 0;
+}
+
+int p4ex02() {
+  cout << "Ushtrimi 2";
+  return 0;
+}
+int p4ex03() {
+  cout << "Ushtrimi 3";
+  return 0;
+}
+
+int p4ex04() {
+  cout << "Ushtrimi 4";
+  return 0;
+}
+
+int p4ex05() {
+  cout << "Ushtrimi 5";
+  return 0;
+}
+
+int p4ex06() {
+  cout << "Ushtrimi 6";
+  return 0;
+}
+
+int p4ex07() {
+  cout << "Ushtrimi 7";
   return 0;
 }
