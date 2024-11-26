0c545526be470ee09df0467c96e640ea27e391fe
arberlisaj <the.arberlisaj@gmail.com>
2024-11-26 23:29:24 +0100

    chore: initialize `pune_laboratori_nr5`

---
+++ include/pune_laboratori_nr5.h
@@ -0,0 +1,23 @@
+#ifndef PUNE_LABORATORI_NR5_H
+#define PUNE_LABORATORI_NR5_H
+
+int p5ex01();
+int p5ex02();
+int p5ex03();
+int p5ex04();
+int p5ex05();
+int p5ex06();
+int p5ex07();
+int p5ex08();
+int p5ex09();
+int p5ex10();
+int p5ex11();
+int p5ex12();
+int p5ex13();
+int p5ex14();
+int p5ex15();
+int p5ex16();
+int p5ex17();
+int p5ex18();
+
+#endif
---
+++ src/pune_laboratori_nr5.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+int p5ex01() {
+  cout << "Ushtrimi 1" << endl;
+  return 0;
+}
+
+int p5ex02() {
+  cout << "Ushtrimi 2" << endl;
+  return 0;
+}
+
+int p5ex03() {
+  cout << "Ushtrimi 3" << endl;
+  return 0;
+}
+
+int p5ex04() {
+  cout << "Ushtrimi 4" << endl;
+  return 0;
+}
+
+int p5ex05() {
+  cout << "Ushtrimi 5" << endl;
+  return 0;
+}
+
+int p5ex06() {
+  cout << "Ushtrimi 6" << endl;
+  return 0;
+}
+
+int p5ex07() {
+  cout << "Ushtrimi 7" << endl;
+  return 0;
+}
+
+int p5ex08() {
+  cout << "Ushtrimi 8" << endl;
+  return 0;
+}
+
+int p5ex09() {
+  cout << "Ushtrimi 9" << endl;
+  return 0;
+}
+
+int p5ex10() {
+  cout << "Ushtrimi 10" << endl;
+  return 0;
+}
+
+int p5ex11() {
+  cout << "Ushtrimi 11" << endl;
+  return 0;
+}
+
+int p5ex12() {
+  cout << "Ushtrimi 12" << endl;
+  return 0;
+}
+
+int p5ex13() {
+  cout << "Ushtrimi 13" << endl;
+  return 0;
+}
+
+int p5ex14() {
+  cout << "Ushtrimi 14" << endl;
+  return 0;
+}
+
+int p5ex15() {
+  cout << "Ushtrimi 15" << endl;
+  return 0;
+}
+
+int p5ex16() {
+  cout << "Ushtrimi 16" << endl;
+  return 0;
+}
+
+int p5ex17() {
+  cout << "Ushtrimi 17" << endl;
+  return 0;
+}
+
+int p5ex18() {
+  cout << "Ushtrimi 18" << endl;
+  return 0;
+}
