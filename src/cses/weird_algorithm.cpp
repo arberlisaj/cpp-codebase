96b57d39497be15f864a0cfd10664e11aa89023a
arberlisaj <the.arberlisaj@gmail.com>
2024-12-21 23:08:01 +0100

    feat(cses): weird algorithm solution

--- src/cses/weird_algorithm.cpp
+++ src/cses/weird_algorithm.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int weird_algorithm() {
-  cout << "Weird Algorithm" << endl;
+  long long n;
+  cin >> n;
+
+  while (n > 1) {
+    cout << n << " ";
+    if (n & 1)
+      n = 3 * n + 1;
+    else
+      n >>= 1;
+  }
+  cout << n << endl;
+
   return 0;
 }
