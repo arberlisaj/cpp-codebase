760dfaf4cd337daee019dfa793dfea32dec25b90
arberlisaj <the.arberlisaj@gmail.com>
2024-12-22 11:59:01 +0100

    chore(labs):  initialize `pune_laboratori_nr4`

---
+++ include/pune_laboratori_nr6.h
@@ -0,0 +1,13 @@
+#ifndef PUNE_LABORATORI_NR6_H
+#define PUNE_LABORATORI_NR6_H
+
+int p6ex01();
+int p6ex02();
+int p6ex03();
+int p6ex04();
+int p6ex05();
+int p6ex06();
+int p6ex07();
+int p6ex08();
+
+#endif
---
+++ src/lab/pune_laboratori_nr6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int p6ex01() {
+  cout << "p6ex01" << endl;
+  return 0;
+}
+
+int p6ex02() {
+  cout << "p6ex02" << endl;
+  return 0;
+}
+
+int p6ex03() {
+  cout << "p6ex03" << endl;
+  return 0;
+}
+
+int p6ex04() {
+  cout << "p6ex04" << endl;
+  return 0;
+}
+
+int p6ex05() {
+  cout << "p6ex05" << endl;
+  return 0;
+}
+
+int p6ex06() {
+  cout << "p6ex06" << endl;
+  return 0;
+}
+
+int p6ex07() {
+  cout << "p6ex07" << endl;
+  return 0;
+}
+
+int p6ex08() {
+  cout << "p6ex08" << endl;
+  return 0;
+}
