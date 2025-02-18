6363b948f5b3fb3b30097279bfcbeb2f995c1cb7
Arber Lisaj <the.arberlisaj@gmail.com>
2025-02-19 00:33:38 +0100

    chore(lab): small changes

--- src/lab/pune_laboratori_nr6.cpp
+++ src/lab/pune_laboratori_nr6.cpp
@@ -5,97 +5,38 @@
 
 using namespace std;
 
+void bubbleSort(int arr[], int n);
+
+// ---------------------------------------------------------------
+
 /*
  * Lab_06_Ushtrimi_1:
- * 1. Formoni kodin per aksesimin e nje vargu me n elemente.
- * 2. Fusni vlera ne varg
- * 3. Printoni vlerat e vargut
- * 4. Gjeni mesataren e vlerave te vargut
- * 5. Gjetja e vleres maksimale/minimale ne varg
- * 6. Gjetja e nje vlere specifike ne varg
- * 7. Gjetja e vlerave mbi nje kufi te caktuar
+ * Formoni nje program qe lexon n elementet e nje vektori dhe gjen numrin e
+ * elementeve pozitive dhe numrin e elementeve negative.
  */
 
 int p6ex01() {
   int n;
-  cout << "Fusni numrin e elementeve te vektorit: \n";
+  cout << "Fusni numrin e elementeve te vektorit: ";
   cin >> n;
 
-  if (n > 0) {
-    int notat[n];
+  int vektori[n];
+  int elementePozitive = 0;
+  int elementeNegative = 0;
 
-    // Leximi i vlerave ne varg
-    cout << "Fusni " << n << " nota:\n";
-    for (int i = 0; i < n; i++) {
-      cin >> notat[i];
+  for (int i = 0; i < n; i++) {
+    cout << "Fusni elementin " << i << ": ";
+    cin >> vektori[i];
+
+    if (vektori[i] > 0) {
+      elementePozitive++;
+    } else if (vektori[i] < 0) {
+      elementeNegative++;
     }
-
-    // Shkrimi i vlerave ne varg
-    cout << "Shfaqja e " << n << " notave:\n";
-    for (int i = 0; i < n; i++) {
-      cout << notat[i] << ", ";
-    }
-
-    // Mesatarja e vlerave te vargut
-    int shuma = 0;
-    float mesatarja;
-    cout << "\nGjetja e mesatares se " << n << " notave:\n";
-    for (int i = 0; i < n; i++) {
-      shuma += notat[i];
-    }
-    mesatarja = shuma / (n * 1.0);
-    cout << "Mesatarja = " << mesatarja << endl;
-
-    // Gjetja e vleres maksimale/minimale te vargut
-    int vlera_min, elementiVogel, indeksi_min, indeksi_max;
-    vlera_min = elementiVogel = notat[0];
-    indeksi_min = indeksi_max = 0;
-
-    cout << "\nGjetja e notes maksimale dhe minimale\n";
-    for (int i = 0; i < n; i++) {
-      if (elementiVogel < notat[i]) {
-        elementiVogel = notat[i];
-        indeksi_max = i;
-      }
-
-      if (vlera_min > notat[i]) {
-        vlera_min = notat[i];
-        indeksi_min = i;
-      }
-    }
-
-    cout << "Nota maksimale = " << elementiVogel
-         << "Pozicioni i notes maksimale = " << indeksi_max << endl
-         << "Nota minimale = " << vlera_min << endl
-         << "Pozicioni i notes minimale = " << indeksi_min << endl;
-
-    // Gjetja e nje vlere specifike ne varg
-    int nota_kerkuar;
-    cout << "Fusni noten qe kerkoni te gjeni: \n";
-    cin >> nota_kerkuar;
-    for (int i = 0; i < n; i++) {
-      if (nota_kerkuar == notat[i]) {
-        cout << "Nota e kerkuar ka index " << i << endl;
-      }
-    }
-
-    // Gjetja e notave mbi mesatare
-    int notat_mbi_mesatare[n / 2 + 1], j = 0;
-    cout << "Gjetja e notave mbi mesatare: \n";
-    for (int i = 0; i < n; i++) {
-      if (mesatarja < notat[i]) {
-        notat_mbi_mesatare[j] = notat[i];
-        j++;
-      }
-    }
-
-    for (int i = 0; i < j; i++) {
-      cout << notat_mbi_mesatare[i] << ", ";
-    }
-
-  } else {
-    cout << "Madhesia e vargut nuk mund te jete negative\n";
   }
+
+  cout << "Numri i elementeve pozitive: " << elementePozitive << endl;
+  cout << "Numri i elementeve negative: " << elementeNegative << endl;
 
   return 0;
 }
@@ -104,77 +45,34 @@ int p6ex01() {
 
 /*
  * Lab_06_Ushtrimi_2:
- * 1. Formoni kodin per aksesimin e nje vargu 2D me n rreshta dhe m kolona.
- * 2. Fusni vlera ne varg
- * 3. Printoni vlerat e vargut
- * 4. Gjeni mesataren e vlerave te vargut
- * 5. Gjetja e vleres maksimale/minimale ne varg
- * 6. Gjetja e nje vlere specifike ne varg
+ * Formoni nje program qe lexon n elementet e nje vektori dhe gjen shumen e
+ * numrave tek dhe prodhimin e numrave cift.
  */
 
 int p6ex02() {
-  int rreshta, kolona;
+  int n;
+  cout << "Fusni numrin e elementeve te vektorit: ";
+  cin >> n;
 
-  // if(rreshta>0 && kolona>0)
-  cout << "Fusni numrin e rreshtave: \n";
-  cin >> rreshta;
-  cout << "Fusni numrin e kolonave: \n";
-  cin >> kolona;
+  int vektori[n];
+  int shumaNumraveTek = 0;
+  int prodhimiNumraveCift = 1;
 
-  // Krijimi i vargut 2D
-  int notat[rreshta][kolona];
+  for (int i = 0; i < n; i++) {
+    do {
+      cout << "Fusni elementin " << i << ": ";
+      cin >> vektori[i];
+    } while (vektori[i] == 0);
 
-  // Aksesimi i elementeve te vargut
-  for (int i = 0; i < rreshta; i++) {
-    for (int j = 0; j < kolona; j++) {
-      notat[i][j];
+    if (vektori[i] % 2 == 0) {
+      prodhimiNumraveCift *= vektori[i];
+    } else {
+      shumaNumraveTek += vektori[i];
     }
   }
 
-  // Vendosja e vlerave ne elementet te vargut
-  cout << "Fusni " << rreshta * kolona << " vlera\n";
-  for (int i = 0; i < rreshta; i++) {
-    for (int j = 0; j < kolona; j++) {
-      cin >> notat[i][j];
-    }
-  }
-
-  // Shfaqja e vlerave te elementeve te vargut
-  cout << "Shfaqja e " << rreshta * kolona << " vlerave\n";
-  for (int i = 0; i < rreshta; i++) {
-    for (int j = 0; j < kolona; j++) {
-      cout << notat[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  // Shfaqja e vlerave te elementeve te vargut
-  int shuma_vek2d = 0;
-  cout << "Shuma e " << rreshta * kolona << " vlerave\n";
-  for (int i = 0; i < rreshta; i++) {
-    for (int j = 0; j < kolona; j++) {
-      shuma_vek2d += notat[i][j];
-    }
-  }
-
-  cout << "Shuma e vlerave te elementeve te vek 2d " << shuma_vek2d << endl;
-
-  // Shuma e Diagonales se Pare
-  int j = 0, shuma_diag_1 = 0;
-  for (int i = 0; i < rreshta; i++) {
-    shuma_diag_1 += notat[i][j];
-    j++;
-  }
-
-  // Shuma e Diagonales se Dyte
-  j = 2;
-  int shuma_diag_2 = 0;
-  for (int i = 0; i < rreshta; i++) {
-    shuma_diag_2 += notat[i][j];
-    j--;
-  }
-  cout << "Shuma e Diagonales se Pare: " << shuma_diag_1 << endl
-       << "Shuma e Diagonales se Dyte: " << shuma_diag_2 << endl;
+  cout << "Shuma e numrave tek: " << shumaNumraveTek << endl;
+  cout << "Prodhimi i numrave cift: " << prodhimiNumraveCift << endl;
 
   return 0;
 }
@@ -184,7 +82,7 @@ int p6ex02() {
 /*
  * Lab_06_Ushtrimi_3:
  * Te formohet vektori a ne menyre qe secili element te jete i barabarte
- * me katrorin e indeksit te tij
+ * me katrorin e indeksit te tij.
  */
 
 int p6ex03() {
@@ -217,8 +115,8 @@ int p6ex03() {
 
 /*
  * Lab_06_Ushtrimi_4:
- * Të formohet vektori a me pesë numrat e parë tek dhe vektori b me pesë numrat
- * e parë çift.
+ * Të formohet vektori a me pesë numrat e parë tek dhe vektori b me pesë
+ * numrat e parë çift.
  */
 
 int p6ex04() {
@@ -250,19 +148,6 @@ int p6ex04() {
 
 // ---------------------------------------------------------------
 
-// Radhit vektorin ne rendin rrites
-void bubbleSort(int arr[], int n) {
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-}
-
 int p6ex05() {
 
   /*
@@ -274,11 +159,11 @@ int p6ex05() {
    * 5.Te llogaritet prodhimi i elementeve te vektorit a.
    * 6.Te llogaritet vlera absolute me e vogel e elementeve te vektorit a dhe
    * indeksi i saj.
-   * 7.Te formohet vektori i ri b nga elementet me indeks tek te vektorit a duke
-   * mbledhur vlerat e elementeve me vleren e indeksit perkates.
-   * 8.Te llogaritet shuma e katroreve te elementeve negative dhe shuma e kubeve
-   * te elementeve pozitive te vektorit a.
-   * 9.Te rradhiten vlerat e elementeve te vektorit a sipas rendit rrites.
+   * 7.Te formohet vektori i ri b nga elementet me indeks tek te vektorit a
+   * duke mbledhur vlerat e elementeve me vleren e indeksit perkates. 8.Te
+   * llogaritet shuma e katroreve te elementeve negative dhe shuma e kubeve te
+   * elementeve pozitive te vektorit a. 9.Te rradhiten vlerat e elementeve te
+   * vektorit a sipas rendit rrites.
    */
 
   int n;
@@ -348,8 +233,8 @@ int p6ex05() {
       vektori_b.push_back(vektori_a[i] + i);
     }
 
-    // LLogarit shumen e katroreve te elementeve negative dhe shuma e kubeve te
-    // elementeve pozitive.
+    // LLogarit shumen e katroreve te elementeve negative dhe shuma e kubeve
+    // te elementeve pozitive.
     if (vektori_a[i] < 0) {
       shumaKatroreveNegativ += pow(vektori_a[i], 2);
     } else if (vektori_a[i] > 0) {
@@ -402,4 +287,17 @@ int p6ex07() {
 int p6ex08() {
   cout << "p6ex08" << endl;
   return 0;
+}
+
+// Radhit vektorin ne rendin rrites
+void bubbleSort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 }
